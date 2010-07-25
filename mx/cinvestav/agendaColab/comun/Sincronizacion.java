b468805d06d5950bd545ef5ec439864e62e919b5
Absol <andremelench@hotmail.com>
2010-07-25 17:07:24 -0500

    Primeras clases comunes

---
+++ mx/cinvestav/agendaColab/comun/Sincronizacion.java
@@ -0,0 +1,22 @@
+package mx.cinvestav.agendaColab.comun;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class Sincronizacion extends Evento {
+
+    @Override
+    public void read(DataInputStream dinput) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void write(DataOutputStream doutput) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getMiTipo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
