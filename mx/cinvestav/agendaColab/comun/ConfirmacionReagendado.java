ad2f291a0cd343eae660c7413805db1f12b4f924
Absol <andremelench@hotmail.com>
2010-08-11 11:27:46 -0500

    cambios a atributos para heredar una clase

---
+++ mx/cinvestav/agendaColab/comun/ConfirmacionReagendado.java
@@ -0,0 +1,21 @@
+package mx.cinvestav.agendaColab.comun;
+
+/**
+ *
+ * @author absol
+ */
+public class ConfirmacionReagendado extends Confirmacion{
+public static final int miTipo = 12;
+
+public class CitaPublica extends Confirmacion {
+public static final int miTipo = 14;
+
+    public int getMiTipo() {
+        return this.miTipo;
+    }
+
+    public String toString() {
+        return "ConfirmacionReagendado{" + "cita=" + cita + "listaUsuarios=" + listaUsuarios + '}';
+    }
+}
+}
